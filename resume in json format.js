c89b00faca1f5b318ee426c18edf43b659223bca
Gokul0201 <105444440+Gokul0201@users.noreply.github.com>
2022-05-15 21:38:40 +0530

    Add files via upload

---
+++ resume in json format.js
@@ -0,0 +1,44 @@
+let myResume={
+    "basics": {
+      "name": "GOKULAKRISHNAN K",
+      "email": "kgokula314@gmail.com",
+      "phone": 9159550219,
+      "degree": "B.E",
+      "location": {
+        "address": "24,pudhupoolamedu,Nanjalur P.O,Chidambaram T.K",
+        "postalCode": 608302,
+        "city": "CHIDAMBARAM",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+    },
+    "work": [
+      {
+        "company": "Prompt Powertech Solutions",
+        "position": "Service Engineer",
+        "startDate": "12-10-2020",
+        "endDate": "up to date",
+      },
+    ],
+    "education": [
+      {
+        "institution": "Jerusalem College Of Engineering",
+        "department": "Electrical and Electronics",
+        "batch ": 2016-2020,
+        "gpa": 6.6,
+      }
+    ],
+    "skills": [
+      {
+        "name": "java,HTML,CSS,JS",
+        "level": "beginner",
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,English"
+      }
+    ],
+   
+  }
+  console.log(myResume);
